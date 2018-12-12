b64f37a47639c00f12f4463b7f540caa67c58d0e
PorchDriver <tantoniw@cogeco.ca>
2018-12-12 18:59:26 -0500

    added keys

--- keys.js
+++ keys.js
@@ -7,3 +7,8 @@ exports.spotify = {
   secret: process.env.SPOTIFY_SECRET
 }
 
+
+//exports.spotify = {
+// id: "4cb744ae476f4da78371c59585e3b2e5",
+//  secret: "fde95638fc624c9e9cc7bbf449da929c"
+//}
